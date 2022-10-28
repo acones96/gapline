b539a6af32cee37184debf071a60142dbd8eb729
Adrian Constandse <aconstandse.es@gmail.com>
2022-10-28 09:15:58 -0500

    Aplicar autoformato
    Se generaron los archivos 'extension.js' y 'extension.js.map' al cambiar el nombre de comando en 'extension.ts'

---
+++ out/extension.js
@@ -0,0 +1,59 @@
+// Utiliza el modo estricto de JavaScript
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+// Importa el modulo de referencia y lo referencia con el alias vscode
+const vscode = require("vscode");
+// Este metodo es llamado cuando la extension es activada
+function activate(context) {
+    // Nombre del parametro debe ser igual al nombre de comando del archivo package.json
+    let disposable = vscode.commands.registerCommand('gapline.lineGapper', () => {
+        // Obtiene el editor activo en VS Code
+        var editor = vscode.window.activeTextEditor;
+        // En caso de no encontrar ningun editor, simplemente retornara
+        if (!editor) {
+            return;
+        }
+        // Selecciona el texto del editor activo
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
+        // Visualiza un cuadro para solicitarnos el numero de lineas donde se insertara la linea en blanco
+        vscode.window.showInputBox({ prompt: 'Lineas?' }).then(value => {
+            // Asigna el valor que ingresamos a la variable 'numerOfLines'
+            let numberOfLines = +value;
+            // Crea un nuevo array
+            var textInChunks = [];
+            // Divide el texto a partir de cada salto de linea en un array
+            text.split('\n').forEach((currentLine, lineIndex) => {
+                // Agrega la linea actual al array 'textInChunks'
+                textInChunks.push(currentLine);
+                // Verifica si el numero de linea no deja residuo en su division, si es asi, ingresa un texto vacio al array
+                if ((lineIndex + 1) % numberOfLines === 0) {
+                    textInChunks.push('');
+                }
+            });
+            // Une el array con un salto de linea de diferencia
+            text = textInChunks.join('\n');
+            // Utiliza la funcion para editar el texto seleccionado
+            editor.edit((editBuilder) => {
+                // Toma un rango del parametro seleccionado
+                var range = new vscode.Range(
+                    // Toma la linea y columna inicial
+                    selection.start.line, 0,
+                    // Toma la linea final
+                    selection.end.line,
+                    // Toma la columna final
+                    editor.document.lineAt(selection.end.line).text.length);
+                // Reemplaza el texto seleccionado con el array que se unio
+                editBuilder.replace(range, text);
+            });
+        });
+    });
+    // Elimina, por decirlo de una forma, los archivos temporales al cerrar la extension
+    context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+// Este metodo se usa cuando se desactiva la extension
+function deactivate() { }
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
