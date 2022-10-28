4d4b6bfe2671060a2e546b2cdb7da61103c44e7b
Adrian Constandse <aconstandse.es@gmail.com>
2022-10-28 09:20:08 -0500

    Nueva configuracion de extension
    * Se aplico el codigo descrito en el documento para la actividad.
    
    * Se comento por linea lo que el codigo realizara
    
    * Se cambio el nombre del comando que ejecutara la extension (linea 10)

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// Utiliza el modo estricto de JavaScript
+'use strict';
+
+// Importa el modulo de referencia y lo referencia con el alias vscode
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Este metodo es llamado cuando la extension es activada
 export function activate(context: vscode.ExtensionContext) {
+    // Nombre del parametro debe ser igual al nombre de comando del archivo package.json
+    let disposable = vscode.commands.registerCommand('gapline.lineGapper', () => {
+        // Obtiene el editor activo en VS Code
+        var editor = vscode.window.activeTextEditor;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gapline" is now active!');
+        // En caso de no encontrar ningun editor, simplemente retornara
+        if (!editor) { return; }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gapline.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Line Gapper!');
-	});
+        // Selecciona el texto del editor activo
+        var selection = editor.selection;
+        var text = editor.document.getText(selection);
 
-	context.subscriptions.push(disposable);
+        // Visualiza un cuadro para solicitarnos el numero de lineas donde se insertara la linea en blanco
+        vscode.window.showInputBox({ prompt: 'Lineas?' }).then(value => {
+
+            // Asigna el valor que ingresamos a la variable 'numerOfLines'
+            let numberOfLines = +value;
+            // Crea un nuevo array
+            var textInChunks: Array<string> = [];
+            // Divide el texto a partir de cada salto de linea en un array
+            text.split('\n').forEach((currentLine: string, lineIndex) => {
+                // Agrega la linea actual al array 'textInChunks'
+                textInChunks.push(currentLine);
+                // Verifica si el numero de linea no deja residuo en su division, si es asi, ingresa un texto vacio al array
+                if ((lineIndex + 1) % numberOfLines === 0) { textInChunks.push(''); }
+            });
+
+            // Une el array con un salto de linea de diferencia
+            text = textInChunks.join('\n');
+
+            // Utiliza la funcion para editar el texto seleccionado
+            editor.edit((editBuilder) => {
+                // Toma un rango del parametro seleccionado
+                var range = new vscode.Range(
+                    // Toma la linea y columna inicial
+                    selection.start.line, 0,
+                    // Toma la linea final
+                    selection.end.line,
+                    // Toma la columna final
+                    editor.document.lineAt(selection.end.line).text.length
+                );
+                // Reemplaza el texto seleccionado con el array que se unio
+                editBuilder.replace(range, text);
+            });
+        })
+    });
+
+    // Elimina, por decirlo de una forma, los archivos temporales al cerrar la extension
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+// Este metodo se usa cuando se desactiva la extension
+export function deactivate() { }
